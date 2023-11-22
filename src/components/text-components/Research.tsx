beb0e4bb386b066028b08c8cbde8d2aefcf82e3c
atryknaav <aetryknaavzaaroyt@gmail.com>
2023-11-22 04:39:57 +0100

    Minor changes

--- src/components/text-components/Research.tsx
+++ src/components/text-components/Research.tsx
@@ -1,5 +1,3 @@
-
-
 const Research = () => {
   return (
     <div className='text-white'>
@@ -13,13 +11,40 @@ const Research = () => {
           <h1 className="text-[1.5rem] font-bold">1.1. How primary school grades display kid's development? </h1>
 
           <div className="text-lg">
-            During the primary school period of life, a kid makes important social connections and gets important skills, such as writing, reading, and counting. And at this age, children should be developing as much, as it's possible, especially it relates to school program. For example, math. If a kid is bad at math in primary school, it will be much more difficult for them to catch in future.
+          Children not only acquire fundamental abilities like reading, writing, and counting in primary school, but they also develop important social skills. This is a critical time for social and intellectual growth, with a focus on arithmetic and other related fields. Math proficiency in these formative years is crucial because it lays the foundation for subsequent learning. Initial assistance is essential to ensuring a strong foundation that will enable kids to confidently tackle future academic problems. The elementary school years are a crucial time for holistic development, which includes social and intellectual intelligence.
+
+          </div>
+
+          <h1 className="text-[1.5rem] font-bold">1.2. Primary school math learning impact researches. </h1>
+
+          <div className="text-lg">
+            There is a study about the impact of primary school math published on <a target='_blank' className="text-unimportant hover:text-[#ffe7e7]" href=" https://research.ebsco.com/c/co6jbk/viewer/pdf/o3uv4yft75"> British Educational Research Journal. </a> <br />
+            The study examined the long-term effects of elementary education on pupils' mathematical achievement at the 17-year-old age. The research aimed to determine the long-term impacts of early education by using cross-classified multilevel models to address the complex data structure where students are linked with both primary and secondary institutions. 
+          </div>
+
+          <div className="text-lg">
+            Remarkably, the findings revealed the important and enduring effects of elementary schools on pupils' mathematical proficiency, even after taking into consideration the diverse backgrounds and past academic records of the students. Essentially, elementary schools shaped pupils' mathematical aptitudes well into their later adolescent years. 2.9% of the unexplained variance in mathematics achievement at age 17 was explained by the elementary school impact.
+          </div>
+
+          <div className="text-lg">
+            Key student background variables taken into account in the analysis were gender, home language, socioeconomic level (SES), and prior academic performance. Notable results showed that boys generally performed better than girls, that a greater socioeconomic status was linked to better mathematical performance, and that pupils who had previously excelled in both language and mathematics had a significant influence on their subsequent math scores.
+          </div>
+
+          <div className="text-lg">
+            The study also looked at particular features of elementary schools that might have an impact on long-term results. The percentage of high-risk pupils, the school's educational network, and the primary school's academic efficacy were all taken into consideration. One important feature that stood out was how well primary schools taught mathematics, which had a beneficial impact on pupils' achievement in the subject by the time they were 17 years old.
+          </div>
+
+          <div className="text-lg">
+            On the whole, the study shows the long-lasting value of early learning experiences and the critical role that primary schools play in determining kids' academic paths, especially in mathematics. The results show that, in order to achieve favorable long-term outcomes, attention must be paid to the effectiveness and quality of elementary education. This has consequences for educational policy and initiatives; just the thing that we are looking for.
+          </div>
+
+            <img src="/table1.png" alt="First table" />
           </div>
   
         </div>
   
   
-    </div>
+    
   )
 }
 
